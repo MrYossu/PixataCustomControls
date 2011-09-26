f81914dd661e3dd5ee7de2750dc304556ff49005
VisualSVN <VisualSVN@acd6a087-dccf-e640-9fde-2a8545d4ea0d>
2011-09-26 18:16:21 +0000

    Added text alignment tothe two stylable label controls

---
+++ PixataCustomControls/PixataCustomControls/PixataCustomControls.Design/Editors/AlignmentPropertyPicker.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Microsoft.LightSwitch.Designers.PropertyPages.ViewModel;
+
+namespace PixataCustomControls.Editors {
+  public partial class AlignmentPropertyPicker : UserControl {
+    public static readonly DependencyProperty ExplorerControlDataContextProperty = DependencyProperty.Register("DummyProperty", typeof(IBindablePropertyEntry), typeof(AlignmentPropertyPicker), new PropertyMetadata(OnExplorerControlDataContextPropertyChanged));
+
+    public AlignmentPropertyPicker() {
+      InitializeComponent();
+      SetBinding(ExplorerControlDataContextProperty, new Binding());
+    }
+
+    private static void OnExplorerControlDataContextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+      AlignmentPropertyPicker EC = (AlignmentPropertyPicker)d;
+      EC.cbComboBox.Items.Add("Left");
+      EC.cbComboBox.Items.Add("Center");
+      EC.cbComboBox.Items.Add("Right");
+    }
+  }
+}
