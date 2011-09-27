dd0fa7ace862eb1c294bd5ac2cb3883261cf48f3
VisualSVN <VisualSVN@acd6a087-dccf-e640-9fde-2a8545d4ea0d>
2011-09-27 17:50:35 +0000

    Added a LabelledGroup control

---
+++ PixataCustomControls/PixataCustomControls/PixataCustomControls.Client/Presentation/Controls/LabelledGroup.xaml.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Resources;
+
+using Microsoft.LightSwitch.Presentation;
+
+namespace PixataCustomControls.Presentation.Controls {
+  public partial class LabelledGroup : UserControl {
+    public LabelledGroup() {
+      InitializeComponent();
+    }
+  }
+
+  [Export(typeof(IControlFactory))]
+  [ControlFactory("PixataCustomControls:LabelledGroup")]
+  internal class LabelledGroupFactory : IControlFactory {
+    #region IControlFactory Members
+
+    public DataTemplate DataTemplate {
+      get {
+        if (null == this.dataTemplate) {
+          this.dataTemplate = XamlReader.Load(LabelledGroupFactory.ControlTemplate) as DataTemplate;
+        }
+        return this.dataTemplate;
+      }
+    }
+
+    public DataTemplate GetDisplayModeDataTemplate(IContentItem contentItem) {
+      return null;
+    }
+
+    #endregion
+
+    #region Private Fields
+
+    private DataTemplate dataTemplate;
+
+    #endregion
+
+    #region Constants
+
+    private const string ControlTemplate =
+        "<DataTemplate" +
+        " xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"" +
+        " xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\"" +
+        " xmlns:ctl=\"clr-namespace:PixataCustomControls.Presentation.Controls;assembly=PixataCustomControls.Client\">" +
+        "<ctl:LabelledGroup/>" +
+        "</DataTemplate>";
+
+    #endregion
+  }
+}
