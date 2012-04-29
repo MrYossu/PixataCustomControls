6e5769c2d26f38b6acc68b1a64fe4e5d01844599
VisualSVN <VisualSVN@acd6a087-dccf-e640-9fde-2a8545d4ea0d>
2012-04-29 15:48:24 +0000

    Added a queue to the toast control

--- PixataCustomControls/PixataCustomControls/PixataCustomControls.Client/Presentation/Controls/PixataToastHelper.cs
+++ PixataCustomControls/PixataCustomControls/PixataCustomControls.Client/Presentation/Controls/PixataToastHelper.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
 using Microsoft.LightSwitch.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,12 @@ namespace PixataCustomControls.Presentation.Controls {
     private const int MaxHeight = 100;
     private const int DefaultHeight = 85;
     private static NotificationWindow nw;
+    private static Queue<Tuple<PixataToastControlInterface, PixataToastControlInformation>> _toastQueue = new Queue<Tuple<PixataToastControlInterface, PixataToastControlInformation>>();
+    private static bool _showingToast;
+
+    public static void ClearToastQueue() {
+      _toastQueue.Clear();
+    }
 
     public static void ShowToast(string TitleText, string MessageText, PixataToastStyles ToastStyle = PixataToastStyles.Plain, int TimeOut = DefaultTimeOut, int Width = DefaultWidth, int Height = DefaultHeight) {
       Dispatchers.Main.BeginInvoke(() => {
@@ -23,21 +32,41 @@ namespace PixataCustomControls.Presentation.Controls {
     public static void ShowToast(string TitleText, string MessageText, PixataToastControlInterface ToastControl, int TimeOut = DefaultTimeOut, int Width = DefaultWidth, int Height = DefaultHeight) {
       if (Application.Current.IsRunningOutOfBrowser) {
         Dispatchers.Main.BeginInvoke(() => {
-          nw = new NotificationWindow();
-          try {
-            // just in case there's already one showing
-            nw.Close();
-          }
-          catch { }
-          ToastControl.Closed += new EventHandler(ToastControl_Closed);
-          nw.Width = Math.Max(Math.Min(Width, MaxWidth), 0);
-          nw.Height = Math.Max(Math.Min(Height, MaxHeight), 0);
           ToastControl.TitleText = TitleText;
           ToastControl.MessageText = MessageText;
-          nw.Content = (FrameworkElement)ToastControl;
-          nw.Show(Math.Max(Math.Min(TimeOut, MaxTimeOut), 0));
+          PixataToastControlInformation info = new PixataToastControlInformation {
+            Height = Math.Max(Math.Min(Height, MaxHeight), 0),
+            Width = Width = Math.Max(Math.Min(Width, MaxWidth), 0),
+            TimeOut = Math.Max(Math.Min(TimeOut, MaxTimeOut), 0)
+          };
+          _toastQueue.Enqueue(new Tuple<PixataToastControlInterface, PixataToastControlInformation>(ToastControl, info));
+          ShowNextToastInQueue();
         });
       }
+    }
+
+    private static void ShowNextToastInQueue() {
+      if (_toastQueue.Count > 0 && !_showingToast) {
+        nw = new NotificationWindow();
+        nw.Closed += new EventHandler(nw_Closed);
+        Tuple<PixataToastControlInterface, PixataToastControlInformation> t = _toastQueue.Dequeue();
+        // Set up the toast
+        PixataToastControlInterface toast = t.Item1;
+        toast.Closed += new EventHandler(ToastControl_Closed);
+        nw.Content = (FrameworkElement)toast;
+        // Set up the meta info
+        PixataToastControlInformation info = t.Item2;
+        nw.Width = info.Width;
+        nw.Height = info.Height;
+        // Now show it
+        _showingToast = true;
+        nw.Show(info.TimeOut);
+      }
+    }
+
+    static void nw_Closed(object sender, EventArgs e) {
+      _showingToast = false;
+      ShowNextToastInQueue();
     }
 
     static void ToastControl_Closed(object sender, EventArgs e) {
@@ -78,6 +107,13 @@ namespace PixataCustomControls.Presentation.Controls {
       Alert, // bell
       Error, //red circle "!"
       Elephant // elephant (just don't ask!)
+    }
+
+    private class PixataToastControlInformation {
+      // Helper class used to store info about the toast. Used when we add the toast to the queue
+      public int TimeOut { get; set; }
+      public int Width { get; set; }
+      public int Height { get; set; }
     }
   }
 }
