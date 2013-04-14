bad672da580044c2b00c88c9476503c917d2d242
VisualSVN <VisualSVN@acd6a087-dccf-e640-9fde-2a8545d4ea0d>
2013-04-14 15:25:30 +0000

    Changed the colour picker so you can type in a specific colour

--- PixataCustomControls/PixataCustomControls/PixataCustomControls.Client.Design/Editors/SilverlightColourPicker/SilverlightColourPicker.xaml.cs
+++ PixataCustomControls/PixataCustomControls/PixataCustomControls.Client.Design/Editors/SilverlightColourPicker/SilverlightColourPicker.xaml.cs
@@ -1,7 +1,30 @@
-﻿namespace PixataCustomControls.Editors.SilverlightColourPicker {
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PixataCustomControls.Editors.SilverlightColourPicker {
   public partial class SilverlightColourPicker {
+    private StringToColourVc vc = new StringToColourVc();
+
     public SilverlightColourPicker() {
       InitializeComponent();
+      TheColourPicker.SelectedColourChanged += TheColourPicker_SelectedColourChanged;
+      ErrorTb.Visibility = Visibility.Collapsed;
+      ColourTb.Text = TheColourPicker.SelectedColour.ToString();
+    }
+
+    private void TheColourPicker_SelectedColourChanged(object sender, SelectedColourEventArgs e) {
+      ColourTb.Text = TheColourPicker.SelectedColour.ToString();
+    }
+
+    private void ColourTb_OnTextChanged(object sender, TextChangedEventArgs e) {
+      Color convertedColour = (Color)vc.Convert(ColourTb.Text, null, null, null);
+      if (convertedColour != Colors.Orange) {
+        ErrorTb.Visibility = Visibility.Collapsed;
+        TheColourPicker.SelectedColour = convertedColour;
+      } else {
+        ErrorTb.Visibility = Visibility.Visible;
+      }
     }
   }
 }
--- PixataCustomControls/PixataCustomControls/PixataCustomControls.Design/Editors/WpfColourPicker/WpfColourPicker.xaml.cs
+++ PixataCustomControls/PixataCustomControls/PixataCustomControls.Design/Editors/WpfColourPicker/WpfColourPicker.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace PixataCustomControls.Editors.WpfColourPicker {
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PixataCustomControls.Editors.WpfColourPicker {
   public partial class WpfColourPicker {
+    private StringToColourVc vc = new StringToColourVc();
+
     public WpfColourPicker() {
       InitializeComponent();
+      TheColourPicker.SelectedColourChanged += TheColourPicker_SelectedColourChanged;
+      ColourTb.Text = TheColourPicker.SelectedColour.ToString();
+    }
+
+    private void TheColourPicker_SelectedColourChanged(object sender, SelectedColourEventArgs e) {
+      ColourTb.Text = TheColourPicker.SelectedColour.ToString();
+    }
+
+    private void ColourTb_OnTextChanged(object sender, TextChangedEventArgs e) {
+      Color convertedColour = (Color)vc.Convert(ColourTb.Text, null, null, null);
+      if (convertedColour != Colors.Orange) {
+        ErrorTb.Visibility = Visibility.Collapsed;
+        TheColourPicker.SelectedColour = convertedColour;
+      } else {
+        ErrorTb.Visibility = Visibility.Visible;
+      }
     }
   }
 }
