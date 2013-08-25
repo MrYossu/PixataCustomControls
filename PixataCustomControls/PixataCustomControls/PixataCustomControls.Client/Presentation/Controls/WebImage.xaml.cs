03924bb14932b055c3519286c922382501abde4b
VisualSVN <VisualSVN@acd6a087-dccf-e640-9fde-2a8545d4ea0d>
2013-08-25 13:33:12 +0000

    Modified the WebImage control so you can get at the underlying Image control from code

--- PixataCustomControls/PixataCustomControls/PixataCustomControls.Client/Presentation/Controls/WebImage.xaml.cs
+++ PixataCustomControls/PixataCustomControls/PixataCustomControls.Client/Presentation/Controls/WebImage.xaml.cs
@@ -1,25 +1,21 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Markup;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Windows.Resources;
-
 using Microsoft.LightSwitch.Presentation;
 
 namespace PixataCustomControls.Presentation.Controls {
-  public partial class WebImage : UserControl {
+  public partial class WebImage : IContentVisual {
     public WebImage() {
       InitializeComponent();
+    }
+
+    public void Show() {
+    }
+
+    public object Control {
+      get {
+        return TheImage;
+      }
     }
   }
 
